dcdc55d84266bda4bea1fb9f186d97cf76b925ca
Toresean <bryan.johns@gmail.com>
2018-12-05 18:32:28 -0600

    recursive directory list

--- filelist.py
+++ filelist.py
@@ -5,10 +5,16 @@ from colorama import Fore, Back, Style
 
 str = input("Enter the path: ")
 
-for fname in os.listdir(str):
-    path = os.path.join(str,fname)
-    if os.path.isdir(path):
-        print (Fore.GREEN + path)
+#for fname in os.listdir(str):
+#    path = os.path.join(str,fname)
+#    if os.path.isdir(path):
+#        print (Fore.GREEN + path)
 
-    else:
-        print (Fore.WHITE + path)
+#    else:
+#        print (Fore.WHITE + path)
+
+
+for root, dirs, files in (os.walk(str,topdown=False)):
+    print('Found directory: %s' % root)
+    for fname in files:
+        print('\t%s' % fname)
