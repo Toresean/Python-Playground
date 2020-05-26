4a454351c89a6472f95f77ed568d5b1e3c389c0e
Toresean <bryan.johns@gmail.com>
2020-05-26 09:03:31 -0500

    Added mars and starship to graphic image

---
+++ escape game/listings1-1.py
@@ -0,0 +1,14 @@
+# Spacewalk
+# Uppercase variables ( height,width means constant value)
+# lowercase variables player means values will change
+
+
+WIDTH= 800
+HEIGHT = 600
+player_x = 600
+player_y=350
+
+def draw():
+    screen.blit(images.backdrop, (0, 0))
+    screen.blit(images.mars,(50,50))
+    screen.blit(images.ship,(130,150))
